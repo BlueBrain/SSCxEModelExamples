b55904430f6a7512b6cc26aa01b5f09391fa92d6
Anil Tuncel <anil.tuncel@epfl.ch>
2022-11-07 17:21:52 +0100

    add somatic validation feature extraction test

---
+++ somatic_validation/tests/test_extract_features.py
@@ -0,0 +1,46 @@
+"""Test the extraction of features required by somatic validation."""
+
+from types import MappingProxyType
+import json
+from pathlib import Path
+
+from ..extract_features import (
+    translate_legacy_targets,
+    get_files_metadata,
+    extract_efeatures,
+)
+
+
+def test_extract_efeatures():
+    """Test the feature values."""
+
+    traces_dir = Path("..") / "feature_extraction" / "input-traces"
+
+    cell_ids = (
+        "C060109A1-SR-C1",
+        "C060109A2-SR-C1",
+        "C060109A3-SR-C1",
+        "C070109A4-C1",
+        "C080501A5-SR-C1",
+        "C080501B2-SR-C1",
+    )
+
+    with open("experiments.json", "r") as f:
+        experiments = MappingProxyType(json.load(f))
+
+    targets = translate_legacy_targets(experiments)
+
+    files_metadata = get_files_metadata(traces_dir, cell_ids, experiments)
+    etype = "test_L5PC"
+    protocols_rheobase = ["IDthresh", "IDRest"]
+    extract_efeatures(
+        etype, files_metadata, targets, protocols_rheobase, plot=False, per_cell=False
+    )
+
+    with open(f"{etype}/features.json", "r") as json_file:
+        results = json.load(json_file)
+
+    with open("tests/data/gt_features_from_all_cells.json", "r") as json_file:
+        ground_truth = json.load(json_file)
+
+    assert results == ground_truth
