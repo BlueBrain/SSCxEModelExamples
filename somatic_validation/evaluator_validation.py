019da313e80e08ea5ce33bd4eabf128c23b834fa
Anil Tuncel <anil.tuncel@epfl.ch>
2022-11-10 14:31:52 +0100

    add initial evaluator validation scripts

---
+++ somatic_validation/evaluator_validation.py
@@ -0,0 +1,189 @@
+
+import os
+import json
+
+from model import create as create_cell
+
+import bluepyopt.ephys as ephys
+
+script_dir = os.path.dirname(__file__)
+config_dir = os.path.join(script_dir, 'config')
+
+
+def define_protocols(path_protocols):
+    """Define protocols"""
+
+    protocol_definitions = json.load(open(path_protocols))
+    protocols = {}
+
+    soma_loc = ephys.locations.NrnSeclistCompLocation(
+        name='soma',
+        seclist_name='somatic',
+        sec_index=0,
+        comp_x=0.5)
+
+    for protocol_name, protocol_definition in protocol_definitions.items():
+        # By default include somatic recording
+        somav_recording = ephys.recordings.CompRecording(
+            name='%s.soma.v' %
+            protocol_name,
+            location=soma_loc,
+            variable='v')
+
+        recordings = [somav_recording]
+        stimuli = []
+        
+        for stimulus_definition in protocol_definition['stimuli']:
+            
+            if protocol_name in ["IDthresh_200","IDRest_200","IV_-100.0"] : 
+                    stimuli.append(ephys.stimuli.NrnSquarePulse(step_amplitude=stimulus_definition['amp'],
+                                                                step_delay=stimulus_definition['delay'],
+                                                                step_duration=stimulus_definition['duration'],
+                                                                location=soma_loc,
+                                                                total_duration=stimulus_definition['totduration']))
+            elif protocol_name in ["APThreshold_200","APThreshold_170","APThreshold_220","APThreshold_250","APThreshold_300","APThreshold_150","APThreshold_400","APThreshold_430","APThreshold_330","APThreshold_390"]: 
+
+                    stimuli.append(ephys.stimuli.NrnRampPulse(
+                    ramp_amplitude_start=stimulus_definition['hypamp'],
+                    ramp_delay=stimulus_definition['ton'],
+                    ramp_amplitude_end=stimulus_definition['amp'],
+                    ramp_duration=stimulus_definition['toff']-stimulus_definition['ton'],
+                    location=soma_loc,
+                    total_duration=stimulus_definition['tend']))
+            else: 
+
+                    stimuli.append(ephys.stimuli.NrnHDPulse(
+                    step_amplitude=stimulus_definition['amp2'],
+                    step_delay=stimulus_definition['ton'],
+                    step_duration=stimulus_definition['toff']-stimulus_definition['ton'],
+                    duration_of_depol1=stimulus_definition['tmid']-stimulus_definition['ton'],
+                    duration_of_depol2=stimulus_definition['toff']-stimulus_definition['tmid2'],
+                    depol=stimulus_definition['amp'],
+                    location=soma_loc,
+                    total_duration=stimulus_definition['tend']))
+        
+
+        protocols[protocol_name] = ephys.protocols.SweepProtocol(
+            protocol_name,
+            stimuli,
+            recordings)
+
+    return protocols
+
+
+def define_fitness_calculator(protocols, path_features):
+    """Define fitness calculator"""
+    
+    feature_definitions = json.load(open(path_features))
+
+    objectives = []
+    for protocol_name, locations in feature_definitions.items():
+        for location, features in locations.items():
+            for efel_feature_name, meanstd in features.items():
+                feature_name = '%s.%s.%s' % (
+                    protocol_name, location, efel_feature_name)
+                recording_names = {'': '%s.%s.v' % (protocol_name, location)}
+                stimulus = protocols[protocol_name].stimuli[0]
+
+                if protocol_name in ["APThreshold_100","APThreshold_170","APThreshold_220","APThreshold_250","APThreshold_200","APThreshold_300","APThreshold_150","APThreshold_400","APThreshold_330","APThreshold_430","APThreshold_390"]:
+                    stim_start = stimulus.ramp_delay
+                else:
+                    stim_start = stimulus.step_delay
+                    
+                if location == 'soma':
+                    threshold = -20
+                elif 'dend' in location:
+                    threshold = -55
+
+                if protocol_name in ["APThreshold_100","APThreshold_170","APThreshold_220","APThreshold_250","APThreshold_200","APThreshold_300","APThreshold_150","APThreshold_400","APThreshold_430","APThreshold_330","APThreshold_390"]:
+                    stim_end = stimulus.ramp_delay + stimulus.ramp_duration
+                else:
+                    stim_end = stimulus.step_delay + stimulus.step_duration
+
+
+                if protocol_name == 'bAP':
+                    stim_end = stimulus.total_duration
+                elif protocol_name in ["APThreshold_100","APThreshold_170","APThreshold_200","APThreshold_250","APThreshold_220","APThreshold_300","APThreshold_150","APThreshold_400","APThreshold_430","APThreshold_330","APThreshold_390"]: 
+                      stim_end = stimulus.ramp_delay + stimulus.ramp_duration
+                else:
+                    stim_end = stimulus.step_delay + stimulus.step_duration
+                
+                features_sAHP=['AHP_depth_abs',
+                             'sag_amplitude',
+                             'sag_ratio1', 
+                             'steady_state_voltage',
+                             'minimum_voltage',
+                             'steady_state_voltage_stimend',
+                             'AHP_depth_abs_slow']
+                
+                if protocol_name[:-4]=="IDhyperpol" and efel_feature_name in ['sag_amplitude','sag_ratio1','minimum_voltage', 'steady_state_voltage_stimend']:              
+                    feature = ephys.efeatures.eFELFeature(
+                        feature_name,
+                        efel_feature_name=efel_feature_name,
+                        recording_names=recording_names,
+                        stim_start=stim_start,
+                        stim_end=stim_start+stimulus.duration_of_depol1,
+                        exp_mean=meanstd[0],
+                        exp_std=meanstd[1],
+                        threshold=threshold)
+                elif protocol_name[:-4]=="sAHP":
+                    if efel_feature_name in features_sAHP:              
+                        feature = ephys.efeatures.eFELFeature(
+                            feature_name,
+                            efel_feature_name=efel_feature_name,
+                            recording_names=recording_names,
+                            stim_start=stim_end-stimulus.duration_of_depol2,
+                            stim_end=stim_end,
+                            exp_mean=meanstd[0],
+                            exp_std=meanstd[1],
+                            threshold=threshold)
+                    else:
+                           feature = ephys.efeatures.eFELFeature(
+                            feature_name,
+                            efel_feature_name=efel_feature_name,
+                            recording_names=recording_names,
+                            stim_start=stim_start+stimulus.duration_of_depol1,
+                            stim_end=stim_end-stimulus.duration_of_depol2,
+                            exp_mean=meanstd[0],
+                            exp_std=meanstd[1],
+                            threshold=threshold)
+                else:
+                    feature = ephys.efeatures.eFELFeature(
+                        feature_name,
+                        efel_feature_name=efel_feature_name,
+                        recording_names=recording_names,
+                        stim_start=stim_start,
+                        stim_end=stim_end,
+                        exp_mean=meanstd[0],
+                        exp_std=meanstd[1],
+                        threshold=threshold)
+                objective = ephys.objectives.SingletonObjective(
+                    feature_name,
+                    feature)
+                #print(objective)
+                objectives.append(objective)
+                
+    fitcalc = ephys.objectivescalculators.ObjectivesCalculator(objectives)
+    return fitcalc
+
+
+def create(morpho_path,path_params, path_features, path_protocols):
+    """Setup"""
+
+    cell = create_cell(morpho_path,path_params)
+    print(morpho_path)
+    fitness_protocols = define_protocols(path_protocols)
+    fitness_calculator = define_fitness_calculator(fitness_protocols,
+                                                   path_features)
+    param_names = [param.name for param in cell.params.values()
+                   if not param.frozen]
+
+    sim = ephys.simulators.NrnSimulator()
+    
+    return ephys.evaluators.CellEvaluator(
+        cell_model=cell,
+        param_names=param_names,
+        fitness_protocols=fitness_protocols,
+        fitness_calculator=fitness_calculator,
+        sim=sim,
+		timeout=500)
---
+++ somatic_validation/stimuli.py
@@ -0,0 +1,459 @@
+"""Stimuli classes"""
+
+"""
+Copyright (c) 2016, EPFL/Blue Brain Project
+
+ This file is part of BluePyOpt <https://github.com/BlueBrain/BluePyOpt>
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License version 3.0 as published
+ by the Free Software Foundation.
+
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+# pylint: disable=W0511
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Stimulus(object):
+
+    """Stimulus protocol"""
+    pass
+
+
+class NrnCurrentPlayStimulus(Stimulus):
+
+    """Current stimulus based on current amplitude and time series"""
+
+    def __init__(self,
+                 time_points=None,
+                 current_points=None,
+                 location=None):
+        """Constructor
+
+        Args:
+            time_points(): time series (ms)
+            current_points(): current series of injected current amplitudes(nA)
+            location(Location): location of stimulus
+        """
+
+        super(NrnCurrentPlayStimulus, self).__init__()
+        self.time_points = time_points
+        self.current_points = current_points
+        self.location = location
+        self.total_duration = max(time_points)
+        self.iclamp = None
+        self.current_vec = None
+        self.time_vec = None
+
+    def instantiate(self, sim=None, icell=None):
+        """Run stimulus"""
+
+        icomp = self.location.instantiate(sim=sim, icell=icell)
+        logger.debug(
+            'Adding current play stimulus to %s', str(self.location))
+
+        self.iclamp = sim.neuron.h.IClamp(
+            icomp.x,
+            sec=icomp.sec)
+        self.current_vec = sim.neuron.h.Vector(self.current_points)
+        self.time_vec = sim.neuron.h.Vector(self.time_points)
+        self.iclamp.dur = self.total_duration
+        self.iclamp.delay = 0
+        self.current_vec.play(
+            self.iclamp._ref_amp,  # pylint:disable=W0212
+            self.time_vec,
+            1,
+            sec=icomp.sec)
+
+    def destroy(self, sim=None):
+        """Destroy stimulus"""
+
+        self.iclamp = None
+        self.time_vec = None
+        self.current_vec = None
+
+    def __str__(self):
+        """String representation"""
+
+        return "Current play at %s" % (self.location)
+
+
+class NrnNetStimStimulus(Stimulus):
+
+    """Current stimulus based on current amplitude and time series"""
+
+    def __init__(self,
+                 locations=None,
+                 total_duration=None,
+                 interval=None,
+                 number=None,
+                 start=None,
+                 noise=0,
+                 weight=1):
+        """Constructor
+
+        Args:
+            location: synapse point process location to connect to
+            interval: time between spikes (ms)
+            number: average number of spikes
+            start: most likely start time of first spike (ms)
+            noise: fractional randomness (0 deterministic,
+                   1 negexp interval distrubtion)
+        """
+
+        super(NrnNetStimStimulus, self).__init__()
+        if total_duration is None:
+            raise ValueError(
+                'NrnNetStimStimulus: Need to specify a total duration')
+        else:
+            self.total_duration = total_duration
+
+        self.locations = locations
+        self.interval = interval
+        self.number = number
+        self.start = start
+        self.noise = noise
+        self.weight = weight
+        self.connections = {}
+
+    def instantiate(self, sim=None, icell=None):
+        """Run stimulus"""
+
+        for location in self.locations:
+            self.connections[location.name] = []
+            for synapse in location.instantiate(sim=sim, icell=icell):
+                netstim = sim.neuron.h.NetStim()
+                netstim.interval = self.interval
+                netstim.number = self.number
+                netstim.start = self.start
+                netstim.noise = self.noise
+                netcon = sim.neuron.h.NetCon(netstim, synapse)
+                netcon.weight[0] = self.weight
+
+                self.connections[location.name].append((netcon, netstim))
+
+    def destroy(self, sim=None):
+        """Destroy stimulus"""
+
+        self.connections = None
+
+    def __str__(self):
+        """String representation"""
+
+        return "Netstim at %s" % ','.join(
+            location
+            for location in self.locations) \
+            if self.locations is not None else "Netstim"
+
+# TODO Add 'current' to the name
+
+
+class NrnSquarePulse(Stimulus):
+
+    """Square pulse current clamp injection"""
+
+    def __init__(self,
+                 step_amplitude=None,
+                 step_delay=None,
+                 step_duration=None,
+                 total_duration=None,
+                 location=None):
+        """Constructor
+
+        Args:
+            step_amplitude (float): amplitude (nA)
+            step_delay (float): delay (ms)
+            step_duration (float): duration (ms)
+            total_duration (float): total duration (ms)
+            location (Location): stimulus Location
+        """
+
+        super(NrnSquarePulse, self).__init__()
+        self.step_amplitude = step_amplitude
+        self.step_delay = step_delay
+        self.step_duration = step_duration
+        self.location = location
+        self.total_duration = total_duration
+        self.iclamp = None
+
+    def instantiate(self, sim=None, icell=None):
+        """Run stimulus"""
+
+        icomp = self.location.instantiate(sim=sim, icell=icell)
+        logger.debug(
+            'Adding square step stimulus to %s with delay %f, '
+            'duration %f, and amplitude %f',
+            str(self.location),
+            self.step_delay,
+            self.step_duration,
+            self.step_amplitude)
+
+        self.iclamp = sim.neuron.h.IClamp(
+            icomp.x,
+            sec=icomp.sec)
+        self.iclamp.dur = self.step_duration
+        self.iclamp.amp = self.step_amplitude
+        self.iclamp.delay = self.step_delay
+
+    def destroy(self, sim=None):
+        """Destroy stimulus"""
+
+        self.iclamp = None
+
+    def __str__(self):
+        """String representation"""
+
+        return "Square pulse amp %f delay %f duration %f totdur %f at %s" % (
+            self.step_amplitude,
+            self.step_delay,
+            self.step_duration,
+            self.total_duration,
+            self.location)
+
+    
+    
+    
+class NrnHDPulse(Stimulus):
+
+    """Square pulse current clamp injection"""
+
+    def __init__(self,
+                 step_amplitude=None,
+                 step_delay=None,
+                 step_duration=None,
+                 total_duration=None,
+                 depol=None,
+                 duration_of_depol1=None,
+                 duration_of_depol2=None,
+                 #hold=None
+                 location=None):
+        """Constructor
+
+        Args:
+            step_amplitude (float): amplitude (nA)
+            step_delay (float): delay (ms)
+            step_duration (float): duration (ms)
+            total_duration (float): total duration (ms)
+            depol (float): amplitude (nA)
+            durration_of_depol (float): duration of depolarization (ms)
+            location (Location): stimulus Location
+        """
+
+        super(NrnHDPulse, self).__init__()
+        self.step_amplitude = step_amplitude
+        self.step_delay = step_delay
+        self.step_duration = step_duration
+        self.location = location
+        self.total_duration = total_duration
+        self.depol=depol
+        self.duration_of_depol1=duration_of_depol1
+        self.duration_of_depol2=duration_of_depol2
+        #self.hold=None
+        self.iclamp = None
+        self.persistent = []  # TODO move this into higher abstract classes
+
+
+    def instantiate(self, sim=None, icell=None):
+        """Run stimulus"""
+
+        icomp = self.location.instantiate(sim=sim, icell=icell)
+        logger.debug(
+            'Adding square step stimulus to %s with delay %f, '
+            'duration %f, and amplitude %f',
+            str(self.location),
+            self.step_delay,
+            self.step_duration,
+            self.step_amplitude)
+        
+        
+         # create vector to store the times at which stim amp changes
+        times = sim.neuron.h.Vector()
+        # create vector to store to which stim amps over time
+        amps = sim.neuron.h.Vector()
+
+        # at time 0.0, current is 0.0
+        times.append(0.0)
+        amps.append(0.0)
+
+        # until time ramp_delay, current is 0.0
+        times.append(self.step_delay)
+        amps.append(0.0)
+
+        # at time ramp_delay, current is ramp_amplitude_start
+        times.append(self.step_delay)
+        amps.append(self.depol)
+
+        # at time ramp_delay+ramp_duration, current is ramp_amplitude_end
+        times.append(self.step_delay + self.duration_of_depol1)
+        amps.append(self.depol)
+
+        # after ramp, current is set 0.0
+        times.append(self.step_delay + self.duration_of_depol1)
+        amps.append(self.step_amplitude-self.depol)
+
+        times.append(self.step_delay + self.step_duration - self.duration_of_depol2)
+        amps.append(self.step_amplitude-self.depol)
+        
+        times.append(self.step_delay + self.step_duration - self.duration_of_depol2)
+        amps.append(self.depol)
+        
+        times.append(self.step_delay + self.step_duration)
+        amps.append(self.depol)
+        
+        times.append(self.step_delay + self.step_duration)
+        amps.append(0.0)
+        
+        times.append(self.total_duration)
+        amps.append(0.0)
+
+        # create a current clamp
+        self.iclamp = sim.neuron.h.IClamp(
+            icomp.x,
+            sec=icomp.sec)
+        self.iclamp.dur = self.total_duration
+
+        # play the above current amplitudes into the current clamp
+        amps.play(self.iclamp._ref_amp, times, 1)  # pylint: disable=W0212
+
+        # Make sure the following objects survive after instantiation
+        self.persistent.append(times)
+        self.persistent.append(amps)
+
+
+
+    def destroy(self, sim=None):
+        """Destroy stimulus"""
+        self.persistent = []
+
+
+        self.iclamp = None
+
+    def __str__(self):
+        """String representation"""
+
+        return "Square pulse amp %f delay %f duration %f totdur %f at %s" % (
+            self.step_amplitude,
+            self.step_delay,
+            self.step_duration,
+            self.total_duration,
+            self.depol,
+            self.duration_of_depol1,
+            self.duration_of_depol2,
+            self.location)
+
+class NrnRampPulse(Stimulus):
+
+    """Ramp current clamp injection"""
+
+    def __init__(self,
+                 ramp_amplitude_start=None,
+                 ramp_amplitude_end=None,
+                 ramp_delay=None,
+                 ramp_duration=None,
+                 total_duration=None,
+                 location=None):
+        """Constructor
+
+        Args:
+            ramp_amplitude_start (float): amplitude at start of ramp (nA)
+            ramp_amplitude_start (float): amplitude at end of ramp (nA)
+            ramp_delay (float): delay of ramp (ms)
+            ramp_duration (float): duration oframp (ms)
+            total_duration (float): total duration (ms)
+            location (Location): stimulus Location
+        """
+
+        super(NrnRampPulse, self).__init__()
+        self.ramp_amplitude_start = ramp_amplitude_start
+        self.ramp_amplitude_end = ramp_amplitude_end
+        self.ramp_delay = ramp_delay
+        self.ramp_duration = ramp_duration
+        self.location = location
+        self.total_duration = total_duration
+        self.iclamp = None
+        self.persistent = []  # TODO move this into higher abstract classes
+
+    def instantiate(self, sim=None, icell=None):
+        """Run stimulus"""
+
+        icomp = self.location.instantiate(sim=sim, icell=icell)
+        logger.debug(
+            'Adding ramp stimulus to %s with delay %f, '
+            'duration %f, amplitude at start %f and end %f',
+            str(self.location),
+            self.ramp_delay,
+            self.ramp_duration,
+            self.ramp_amplitude_start,
+            self.ramp_amplitude_end
+        )
+
+        # create vector to store the times at which stim amp changes
+        times = sim.neuron.h.Vector()
+        # create vector to store to which stim amps over time
+        amps = sim.neuron.h.Vector()
+
+        # at time 0.0, current is 0.0
+        times.append(0.0)
+        amps.append(0.0)
+
+        # until time ramp_delay, current is 0.0
+        times.append(self.ramp_delay)
+        amps.append(0.0)
+
+        # at time ramp_delay, current is ramp_amplitude_start
+        times.append(self.ramp_delay)
+        amps.append(self.ramp_amplitude_start)
+
+        # at time ramp_delay+ramp_duration, current is ramp_amplitude_end
+        times.append(self.ramp_delay + self.ramp_duration)
+        amps.append(self.ramp_amplitude_end)
+
+        # after ramp, current is set 0.0
+        times.append(self.ramp_delay + self.ramp_duration)
+        amps.append(0.0)
+
+        times.append(self.total_duration)
+        amps.append(0.0)
+
+        # create a current clamp
+        self.iclamp = sim.neuron.h.IClamp(
+            icomp.x,
+            sec=icomp.sec)
+        self.iclamp.dur = self.total_duration
+
+        # play the above current amplitudes into the current clamp
+        amps.play(self.iclamp._ref_amp, times, 1)  # pylint: disable=W0212
+
+        # Make sure the following objects survive after instantiation
+        self.persistent.append(times)
+        self.persistent.append(amps)
+
+    def destroy(self, sim=None):
+        """Destroy stimulus"""
+
+        # Destroy all persistent objects
+        self.persistent = []
+        self.iclamp = None
+
+    def __str__(self):
+        """String representation"""
+
+        return "Ramp pulse amp_start %f amp_end %f delay %f duration %f " \
+            "totdur %f at %s" % (
+                self.ramp_amplitude_start,
+                self.ramp_amplitude_end,
+                self.ramp_delay,
+                self.ramp_duration,
+                self.total_duration,
+                self.location)
